83084a6c7d3ceebaf349248f83b54f5855b5f42d
Joel Varty <joel@agilitycms.com>
2021-06-16 16:47:47 -0400

    debug

--- pages/[...slug].jsx
+++ pages/[...slug].jsx
@@ -5,38 +5,47 @@ import SiteHeader from "components/common/SiteHeader";
 
 export async function getServerSideProps({ params, req, res, query, preview, resolvedUrl, locale, locales, defaultLocale }) {
 
-	if (! preview) {
-		res.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate=59')
+	try {
+
+		if (!preview) {
+			res.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate=59')
+		}
+
+		//import { getAgilityPageProps  } from "@agility/nextjs/node";
+		let { getAgilityPageProps } = require("@agility/nextjs/node")
+
+
+		//place all global here
+		const globalComponents = {
+			header: SiteHeader,
+		};
+
+		const agilityProps = await getAgilityPageProps({
+			preview,
+			params,
+			locale,
+			getModule,
+			defaultLocale,
+			globalComponents,
+		});
+
+		if (!agilityProps) {
+			// We throw to make sure this fails at build time as this is never expected to happen
+			throw new Error(`Page not found`);
+		}
+
+
+		return {
+			// return all props
+			props: agilityProps
+		};
+	} catch (error) {
+		return {
+			props: {
+				error: `An error occurred: ${error}`
+			}
+		}
 	}
-
-	//import { getAgilityPageProps  } from "@agility/nextjs/node";
-	let { getAgilityPageProps } = require("@agility/nextjs/node")
-
-
-	//place all global here
-	const globalComponents = {
-		header: SiteHeader,
-	};
-
-	const agilityProps = await getAgilityPageProps({
-		preview,
-		params,
-		locale,
-		getModule,
-		defaultLocale,
-		globalComponents,
-	});
-
-	if (!agilityProps) {
-		// We throw to make sure this fails at build time as this is never expected to happen
-		throw new Error(`Page not found`);
-	}
-
-
-	return {
-		// return all props
-		props: agilityProps
-	};
 }
 
 // // Next.js will statically pre-render all the paths from Agility CMS
@@ -55,7 +64,8 @@ export async function getServerSideProps({ params, req, res, query, preview, res
 // }
 
 const AgilityPage = (props) => {
-	return <Layout {...props} />;
+	return JSON.stringify(props, )
+	//return <Layout {...props} />;
 };
 
 export default AgilityPage;

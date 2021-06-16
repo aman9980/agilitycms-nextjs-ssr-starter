8712db8004990cfc6c54e91de0c26e086958ca1e
Joel Varty <joel@agilitycms.com>
2021-06-16 17:41:55 -0400

    added env yikes!

--- pages/[...slug].jsx
+++ pages/[...slug].jsx
@@ -7,14 +7,6 @@ export async function getServerSideProps({ params, req, res, query, preview, res
 
 	try {
 
-		process.env.AGILITY_GUID="8a4376e4-u"
-		process.env.AGILITY_API_FETCH_KEY="defaultlive.50dbc3b0b922c1ac7e02f1bba112c85fd1e88848240b9d2371682415e179aeaf"
-		process.env.AGILITY_API_PREVIEW_KEY="defaultpreview.fa2cc7b05bebbb8513814a752b8e3f144911e7aecdb3b996c50daeda83941f96"
-		process.env.AGILITY_SECURITY_KEY="cgBnAFEATwBDAGUAbABiAC0ANgA5ADgAMgA2ADgAMQA2AC0AQQBEADAARAAtADQAOAA0AEIALQBCADMAQgAyAC0ARgA5ADYAQgA1ADAARABBADkAQgA3AEYA"
-
-
-		process.env.AGILITY_LOCALES="en-us"
-		process.env.AGILITY_SITEMAP="website"
 
 
 		if (!preview) {
@@ -74,8 +66,11 @@ export async function getServerSideProps({ params, req, res, query, preview, res
 // }
 
 const AgilityPage = (props) => {
-	return JSON.stringify(props, )
-	//return <Layout {...props} />;
+	if (props.error) {
+		return JSON.stringify(props)
+	} else {
+		return <Layout {...props} />;
+	}
 };
 
 export default AgilityPage;
